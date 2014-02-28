a2891405084a16f71d6874f6ceb979abb26a2229
withc <withc@aliyun.com>
2014-02-28 15:00:40 +0800

    clean code

--- search/load/factory.py
+++ search/load/factory.py
@@ -3,7 +3,6 @@ import load.feature
 
 def featureFactory( name, loader ):
     
-    
     if name == 'tomtom':
         import load.tomtom
         loader.add_process( load.feature.CStartProcess() )
@@ -12,6 +11,15 @@ def featureFactory( name, loader ):
         loader.add_feature( load.tomtom.feat_poi.CPoi( ) )
         loader.add_feature( load.tomtom.feat_link.CLink( ) )
         
+    elif name == 'globetech':
+        import  load.globetech
+        
+        loader.add_process( load.feature.CStartProcess() )
+        loader.add_process( load.feature.CEndProcess() )
+        loader.add_feature( load.globetech.feat_place.CPlace( ) )
+        loader.add_feature( load.globetech.feat_poi.CPoi( ) )
+        loader.add_feature( load.globetech.feat_link.CLink( ) )
+        pass    
     elif name == 'rdf':
         import load.rdf
         pass
