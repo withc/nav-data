ae1f1cf6241b237f45c6ab342383d365d572620a
withc <withc@aliyun.com>
2014-03-03 15:01:23 +0800

    clean code

--- search/load/globetech/feat_place.py
+++ search/load/globetech/feat_place.py
@@ -23,8 +23,7 @@ class CPlace(load.feature.CFeature):
         
         sqlcmd = '''
                     insert into mid_feat_key( feat_type, org_id1, org_id2 )
-                    select distinct
-                           case org_id2
+                    select case org_id2
                              when 1 then 3002
                              when 2 then 3009
                              when 3 then 3010
@@ -33,6 +32,7 @@ class CPlace(load.feature.CFeature):
                             org_id1,
                             org_id2
                        from temp_admincode
+                      order by type, prov_code, amp_code, tam_code
                  '''
         self.db.execute( sqlcmd )
         
@@ -47,6 +47,11 @@ class CPlace(load.feature.CFeature):
         self.db.execute( sqlcmd )
     
     def _domake_geomtry(self):
+        # add the country point by myself
+        self.db.execute( ''' insert into temp_feat_geom( key, type, code, geotype, geom )
+                             select key, type, 7379, 'P',  ST_SetSRID(st_makepoint(100.5018272, 13.7541276), 4326)
+                               from mid_place where type = 3001
+                         ''' )
         sqlcmd = '''
              insert into temp_feat_geom( key, type, code, geotype, geom )
                   select fe.feat_key, fe.feat_type, 7379, 'P', a.the_geom
@@ -62,6 +67,15 @@ class CPlace(load.feature.CFeature):
         self.db.execute( sqlcmd )
         
     def _domake_name(self):
+        # add the country name by myself
+        self.db.execute( ''' insert into temp_feat_name( key, type, nametype, langcode, name )
+                             select key, type, 'ON', 'THA', '\xe0\xb9\x84\xe0\xb8\x97\xe0\xb8\xa2'
+                               from mid_place where type = 3001
+                              union
+                             select key, type, 'ON', 'ENG', 'Thailand'
+                               from mid_place where type = 3001
+                         ''' )
+        
         sqlcmd = '''
              insert into temp_feat_name( key, type, nametype, langcode, name )
                 with ad ( key, type, namt, name )
@@ -85,6 +99,11 @@ class CPlace(load.feature.CFeature):
         pass
         
     def _domake_relation(self):
+        # add the country admin by myself
+        self.db.execute( ''' insert into mid_place_admin( key, type, a0, a1, a2, a7, a8, a9 )
+                             select key, type, key, 0,0,0,0,0
+                               from mid_place where type = 3001
+                         ''' )
         sqlcmd = '''
               insert into mid_place_admin( key, type, a0, a1, a2, a7, a8, a9 )
               with ad ( f_key, f_type, type, prov_code, amp_code, tam_code )
@@ -94,18 +113,18 @@ class CPlace(load.feature.CFeature):
                     join mid_feat_key     as fe
                       on ta.org_id1 = fe.org_id1 and ta.org_id2 = fe.org_id2
                     ) 
-                select a1.f_key, a1.f_type, 0, a1.f_key, 0, 0, 0, 0
-                  from ad as a1
-                 where a1.type = 1
-                union
-                select  a8.f_key, a8.f_type, 0, a1.f_key, 0, 0,a8.f_key, 0
+              select a1.f_key, a1.f_type, ( select key from mid_place where type = 3001 ) , a1.f_key, 0, 0, 0, 0
+                from ad as a1
+               where a1.type = 1
+               union
+              select  a8.f_key, a8.f_type, ( select key from mid_place where type = 3001 ) , a1.f_key, 0, 0,a8.f_key, 0
                 from ad as a8
                 join ad as a1
                   on a1.type      = 1            and 
                      a1.prov_code = a8.prov_code
                where a8.type      = 2
-                union
-                select  a9.f_key, a9.f_type, 0, a1.f_key, 0, 0,a8.f_key, a9.f_key
+               union
+              select  a9.f_key, a9.f_type, ( select key from mid_place where type = 3001 ) , a1.f_key, 0, 0,a8.f_key, a9.f_key
                 from ad as a9
                 join ad as a1
                   on a1.type      = 1            and 
