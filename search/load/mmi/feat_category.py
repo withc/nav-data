93f70e04b42db7a6a97901d670f945ad9799a2df
withc <withc@aliyun.com>
2014-03-04 09:27:43 +0800

    add other vendor module

---
+++ search/load/mmi/feat_category.py
@@ -0,0 +1,24 @@
+import load.feature
+
+class CPoiCategory(load.feature.CFeature):
+    def __init__(self ):
+        print "mmi's poi category"
+        load.feature.CFeature.__init__(self, 'category')
+ 
+    def _domake_key(self):
+        pass
+        
+    def _domake_feature(self):
+        pass
+    
+    def _domake_geomtry(self):
+        pass
+        
+    def _domake_name(self):
+        pass
+    
+    def _domake_attribute(self):
+        pass
+        
+    def _domake_relation(self):
+        pass
